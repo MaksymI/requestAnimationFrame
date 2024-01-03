49e208b2d5f67999389b0212e2734df75ad22c61
MaksymI <maksym.izmailov.lv@gmail.com>
2024-01-03 15:55:09 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const zone = document.querySelector(".zone");
 const dropTemplate = document.createElement("div");
+dropTemplate.classList.add("drop");
+
+const maxSize = 400;
 
 let counter = 1;
 
@@ -11,3 +14,30 @@ function makeDrop(x, y) {
   counter += 1;
   zone.appendChild(newDrop);
 }
+
+zone.addEventListener("click", (e) => {
+  console.log("click - ", e);
+  makeDrop(e.offsetX, e.offsetY);
+});
+
+function tick() {
+  Array.from(zone.querySelectorAll(".drop")).forEach((drop) => {
+    const currentSize = parseInt(getComputedStyle(drop).width, 10);
+    const newSize = currentSize + 1 + Math.random() * 6;
+    drop.style.width = `${newSize}px`;
+    drop.style.height = `${newSize}px`;
+    drop.style.opacity = (maxSize - newSize) / maxSize;
+    if (newSize > maxSize) {
+      drop.remove();
+    }
+  });
+}
+
+function loop() {
+  window.requestAnimationFrame(() => {
+    tick();
+    loop();
+  });
+}
+
+loop();
